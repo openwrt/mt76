7cd57c20a5b676b7dbd97570062e5c33a18d4de1
Felix Fietkau <nbd@openwrt.org>
2015-03-18 14:04:56 +0100

    mac: increase TXOP threshold for autogenerated frames
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- init.c
+++ init.c
@@ -119,6 +119,7 @@ mt76_write_mac_initvals(struct mt76_dev *dev)
 		{ MT_HT_FBK_TO_LEGACY,		0x00001818 },
 		{ MT_VHT_HT_FBK_CFG1,		0xedcba980 },
 		{ MT_PROT_AUTO_TX_CFG,		0x00830083 },
+		{ MT_HT_CTRL_CFG,		0x000001ff },
 	};
 
 	mt76_write_reg_pairs(dev, vals, ARRAY_SIZE(vals));
