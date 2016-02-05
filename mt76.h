707c918966b27c0351fcd3e4d6b57fb0fedf6760
Felix Fietkau <nbd@openwrt.org>
2016-02-05 01:15:58 +0100

    tx: increase ring size
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- mt76.h
+++ mt76.h
@@ -22,7 +22,7 @@
 #include "util.h"
 
 #define MT_RX_RING_SIZE     128
-#define MT_TX_RING_SIZE     128
+#define MT_TX_RING_SIZE     256
 #define MT_MCU_RING_SIZE    32
 #define MT_RX_BUF_SIZE      2048
 
