ba004c0067c8a4b31c88d95ba97c3ec6b4e68bc3
Felix Fietkau <nbd@openwrt.org>
2016-02-05 17:37:09 +0100

    mac80211: add A-MSDU tx support
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- mac80211.c
+++ mac80211.c
@@ -185,6 +185,7 @@ int mt76_register_device(struct mt76_dev *dev, bool vht,
 	wiphy->features |= NL80211_FEATURE_ACTIVE_MONITOR;
 
 	hw->txq_data_size = sizeof(struct mt76_txq);
+	hw->max_tx_amsdu_subframes = 16;
 
 	ieee80211_hw_set(hw, SIGNAL_DBM);
 	ieee80211_hw_set(hw, PS_NULLFUNC_STACK);
@@ -195,6 +196,8 @@ int mt76_register_device(struct mt76_dev *dev, bool vht,
 	ieee80211_hw_set(hw, SUPPORT_FAST_XMIT);
 	ieee80211_hw_set(hw, SUPPORTS_CLONED_SKBS);
 	ieee80211_hw_set(hw, SUPPORTS_AMSDU_IN_AMPDU);
+	ieee80211_hw_set(hw, TX_AMSDU);
+	ieee80211_hw_set(hw, TX_FRAG_LIST);
 
 	if (dev->cap.has_2ghz) {
 		ret = mt76_init_sband_2g(dev, rates, n_rates);
