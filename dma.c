9abb4b175437babdbb4e92d3e584f33566dedb8b
Felix Fietkau <nbd@openwrt.org>
2016-02-08 14:10:04 +0100

    dma: respect buffer offset for fragments
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- dma.c
+++ dma.c
@@ -280,6 +280,7 @@ mt76_add_fragment(struct mt76_dev *dev, struct mt76_queue *q, void *data,
 	int offset = data - page_address(page);
 	struct sk_buff *skb = q->rx_head;
 
+	offset += q->buf_offset;
 	skb_add_rx_frag(skb, skb_shinfo(skb)->nr_frags, page, offset, len,
 			q->buf_size);
 
