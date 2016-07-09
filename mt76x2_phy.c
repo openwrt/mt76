9a75c6caa0dc3478c72e190860c93b6efb0d5d4e
Lorenzo Bianconi <lorenzo.bianconi83@gmail.com>
2016-07-09 14:04:20 +0200

    mt76x2: phy: fix typo in rate-power configuration
    fix typo in MT_TX_PWR_CFG_7 configuration for OFDM_54M max
    tx power adjustment
    
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi83@gmail.com>

--- mt76x2_phy.c
+++ mt76x2_phy.c
@@ -160,7 +160,7 @@ void mt76x2_phy_set_txpower(struct mt76x2_dev *dev)
 	mt76_wr(dev, MT_TX_PWR_CFG_4,
 		mt76x2_tx_power_mask(t.ht[4], t.ht[6], 0, 0));
 	mt76_wr(dev, MT_TX_PWR_CFG_7,
-		mt76x2_tx_power_mask(t.ofdm[4], t.vht[8], t.ht[6], t.vht[8]));
+		mt76x2_tx_power_mask(t.ofdm[6], t.vht[8], t.ht[6], t.vht[8]));
 	mt76_wr(dev, MT_TX_PWR_CFG_8,
 		mt76x2_tx_power_mask(t.ht[14], t.vht[8], t.vht[8], 0));
 	mt76_wr(dev, MT_TX_PWR_CFG_9,
