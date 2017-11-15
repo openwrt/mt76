d8140b6f409ae41c3999200e55b2a04699b574f7
Felix Fietkau <nbd@nbd.name>
2017-11-15 18:07:39 +0100

    mt76x2: clear per-WCID tx rate lookup register
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt76x2_mac.c
+++ mt76x2_mac.c
@@ -536,6 +536,9 @@ void mt76x2_mac_wcid_setup(struct mt76x2_dev *dev, u8 idx, u8 vif_idx, u8 *mac)
 
 	mt76_wr(dev, MT_WCID_ATTR(idx), attr);
 
+	mt76_wr(dev, MT_WCID_TX_RATE(idx), 0);
+	mt76_wr(dev, MT_WCID_TX_RATE(idx) + 4, 0);
+
 	if (idx >= 128)
 		return;
 
