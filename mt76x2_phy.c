efd7724b1a45b4d1ff07c4b1d230b3c2912080ca
Felix Fietkau <nbd@nbd.name>
2017-11-16 18:31:06 +0100

    mt76x2: sync channel gain value with latest reference driver
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt76x2_phy.c
+++ mt76x2_phy.c
@@ -446,8 +446,11 @@ mt76x2_phy_update_channel_gain(struct mt76x2_dev *dev)
 		else
 			gain_delta = 14;
 	} else {
-		mt76_wr(dev, MT_BBP(AGC, 35), 0x11111116);
-		mt76_wr(dev, MT_BBP(AGC, 37), 0x1010161C);
+		if (dev->mt76.chandef.width == NL80211_CHAN_WIDTH_80)
+			mt76_wr(dev, MT_BBP(AGC, 35), 0x10101014);
+		else
+			mt76_wr(dev, MT_BBP(AGC, 35), 0x11111116);
+		mt76_wr(dev, MT_BBP(AGC, 37), 0x2121262C);
 		gain_delta = 0;
 	}
 
