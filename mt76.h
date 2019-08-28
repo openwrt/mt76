202776352b0ace013316eaeb58ffd298a3a80143
Felix Fietkau <nbd@nbd.name>
2019-08-28 11:28:36 +0200

    mt76: report rx a-mpdu subframe status
    This can be used in monitor mode to figure out which subframes were sent as
    part of which A-MPDU
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt76.h
+++ mt76.h
@@ -437,6 +437,7 @@ struct mt76_dev {
 	spinlock_t rx_lock;
 	struct napi_struct napi[__MT_RXQ_MAX];
 	struct sk_buff_head rx_skb[__MT_RXQ_MAX];
+	u32 ampdu_ref;
 
 	struct list_head txwi_cache;
 	struct mt76_sw_queue q_tx[__MT_TXQ_MAX];
@@ -512,6 +513,8 @@ struct mt76_rx_status {
 	struct mt76_wcid *wcid;
 
 	unsigned long reorder_time;
+
+	u32 ampdu_ref;
 
 	u8 iv[6];
 
