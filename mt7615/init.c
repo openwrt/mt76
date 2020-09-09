17ba3432f5afc76b3ac1858ae7d9233a69923748
Felix Fietkau <nbd@nbd.name>
2020-09-09 11:30:57 +0200

    Revert "mt76: mt7615: unlock dfs bands"
    This reverts commit b647180c86bed96e08d86b233efeb139919e3aae.
    Now enabled properly by a separate interface combination
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt7615/init.c
+++ mt7615/init.c
@@ -240,12 +240,6 @@ static const struct ieee80211_iface_combination if_comb[] = {
 		.max_interfaces = 4,
 		.num_different_channels = 1,
 		.beacon_int_infra_match = true,
-		.radar_detect_widths = BIT(NL80211_CHAN_WIDTH_20_NOHT) |
-				       BIT(NL80211_CHAN_WIDTH_20) |
-				       BIT(NL80211_CHAN_WIDTH_40) |
-				       BIT(NL80211_CHAN_WIDTH_80) |
-				       BIT(NL80211_CHAN_WIDTH_160) |
-				       BIT(NL80211_CHAN_WIDTH_80P80),
 	}
 };
 
