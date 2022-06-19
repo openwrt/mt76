899d192e8a79e15f36ab719bcd1a31ad092e17f3
Lorenzo Bianconi <lorenzo@kernel.org>
2022-06-19 22:42:37 +0200

    mt76: move mt7615_txp_ptr in mt76_connac module
    Since mt7615_txp_ptr is shared between mt7615 and mt7921 move it in
    mt76_connac module.
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt7921/pci.c
+++ mt7921/pci.c
@@ -230,7 +230,7 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 {
 	static const struct mt76_driver_ops drv_ops = {
 		/* txwi_size = txd size + txp size */
-		.txwi_size = MT_TXD_SIZE + sizeof(struct mt7921_txp_common),
+		.txwi_size = MT_TXD_SIZE + sizeof(struct mt76_connac_txp_common),
 		.drv_flags = MT_DRV_TXWI_NO_FREE | MT_DRV_HW_MGMT_TXQ,
 		.survey_flags = SURVEY_INFO_TIME_TX |
 				SURVEY_INFO_TIME_RX |
