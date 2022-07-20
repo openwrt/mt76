67fbdb7bed900d0ab3968830825b4d26197f86b3
Sean Wang <sean.wang@mediatek.com>
2022-07-21 06:25:37 +0800

    wifi: mt76: mt7921e: fix race issue between reset and suspend/resume
    It is unexpected that the reset work is running simultaneously with
    the suspend or resume context and it is possible that reset work is still
    running even after mt7921 is suspended if we don't fix the race issue.
    
    Thus, the suspend procedure should be waiting until the reset is completed
    at the beginning and ignore the subsequent the reset requests.
    
    In case there is an error that happens during either suspend or resume
    handler, we will schedule a reset task to recover the error before
    returning the error code to ensure we can immediately fix the error there.
    
    Fixes: 0c1ce9884607 ("mt76: mt7921: add wifi reset support")
    Co-developed-by: YN Chen <YN.Chen@mediatek.com>
    Signed-off-by: YN Chen <YN.Chen@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt7921/pci.c
+++ mt7921/pci.c
@@ -363,6 +363,7 @@ static int mt7921_pci_suspend(struct pci_dev *pdev, pm_message_t state)
 	int i, err;
 
 	pm->suspended = true;
+	flush_work(&dev->reset_work);
 	cancel_delayed_work_sync(&pm->ps_work);
 	cancel_work_sync(&pm->wake_work);
 
@@ -428,6 +429,9 @@ restore_napi:
 restore_suspend:
 	pm->suspended = false;
 
+	if (err < 0)
+		mt7921_reset(&dev->mt76);
+
 	return err;
 }
 
@@ -446,7 +450,7 @@ static int mt7921_pci_resume(struct pci_dev *pdev)
 
 	err = mt7921_mcu_drv_pmctrl(dev);
 	if (err < 0)
-		return err;
+		goto failed;
 
 	mt7921_wpdma_reinit_cond(dev);
 
@@ -476,10 +480,11 @@ static int mt7921_pci_resume(struct pci_dev *pdev)
 		mt76_connac_mcu_set_deep_sleep(&dev->mt76, false);
 
 	err = mt76_connac_mcu_set_hif_suspend(mdev, false);
-	if (err)
-		return err;
-
+failed:
 	pm->suspended = false;
+
+	if (err < 0)
+		mt7921_reset(&dev->mt76);
 
 	return err;
 }
