f52580b07430ccfdcbf81c9e369908e485cdd991
DroidMAX-Tang <59656689+DroidMAX-Tang@users.noreply.github.com>
2022-08-25 12:56:14 +0800

    Create usb.c
    Signed-off-by: DroidMAX-Tang <59656689+DroidMAX-Tang@users.noreply.github.com>

--- mt76x2/usb.c
+++ mt76x2/usb.c
@@ -21,7 +21,8 @@ static const struct usb_device_id mt76x2u_device_table[] = {
 	{ USB_DEVICE(0x0846, 0x9053) },	/* Netgear A6210 */
 	{ USB_DEVICE(0x045e, 0x02e6) },	/* XBox One Wireless Adapter */
 	{ USB_DEVICE(0x045e, 0x02fe) },	/* XBox One Wireless Adapter */
-	{ },
+	{ USB_DEVICE(0x0e8d, 0x7662) },	/* HC-M7662UN */
+        { },
 };
 
 static int mt76x2u_probe(struct usb_interface *intf,
