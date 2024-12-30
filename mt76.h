1884f568ba02231ec1bc0559d5cb4ecea43967aa
Felix Fietkau <nbd@nbd.name>
2024-12-30 18:40:22 +0100

    wifi: mt76: do not add wcid entries to sta poll list during MCU reset
    Fixes linked list management issues during restart, where sta entries
    can be recreated and reinitialized while still in the poll list, triggering
    kernel warnings.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt76.h
+++ mt76.h
@@ -1831,6 +1831,7 @@ mt76_token_put(struct mt76_dev *dev, int token)
 
 void mt76_wcid_init(struct mt76_wcid *wcid, u8 band_idx);
 void mt76_wcid_cleanup(struct mt76_dev *dev, struct mt76_wcid *wcid);
+void mt76_wcid_add_poll(struct mt76_dev *dev, struct mt76_wcid *wcid);
 
 static inline void
 mt76_vif_init(struct ieee80211_vif *vif, struct mt76_vif_data *mvif)
