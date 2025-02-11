687e2fdfbf40e68802cfac2b2a896d5b27fed37a
Razvan Grigore <razvan.grigore@vampirebyte.ro>
2025-02-11 08:12:45 +0000

    wifi: mt76: mt7925: fix returned txpower
    Similar to the mt7921, seems the code was copy-paste from there. This
    bug was noticed by me and confirmed by many more people using this
    chipset with openwrt: https://github.com/openwrt/mt76/issues/783
    
    It seems that for this particular chipset the mt76_phy->txpower_cur is
    never set, like in the case of other chipsets like mt7603, mt7615, etc.
    
    The driver is correctly calculating the TX power adjustment based on the
    number of antennas. However, the base TX power (phy->txpower_cur) is not
    being properly initialized or set to a meaningful value. It's starting
    at 0, so the final result is just the antenna gain compensation divided
    by 2.
    
    Signed-off-by: Razvan Grigore <razvan.grigore@vampirebyte.ro>
    Link: https://patch.msgid.link/20250211081247.5892-4-razvan.grigore@vampirebyte.ro
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt7925/main.c
+++ mt7925/main.c
@@ -1726,7 +1726,9 @@ static void mt7925_ipv6_addr_change(struct ieee80211_hw *hw,
 int mt7925_set_tx_sar_pwr(struct ieee80211_hw *hw,
 			  const struct cfg80211_sar_specs *sar)
 {
+	struct mt76_power_limits limits_array;
 	struct mt76_phy *mphy = hw->priv;
+	int tx_power;
 
 	if (sar) {
 		int err = mt76_init_sar_power(hw, sar);
@@ -1735,6 +1737,11 @@ int mt7925_set_tx_sar_pwr(struct ieee80211_hw *hw,
 			return err;
 	}
 	mt792x_init_acpi_sar_power(mt792x_hw_phy(hw), !sar);
+
+	tx_power = mt76_get_power_bound(mphy, hw->conf.power_level);
+	tx_power = mt76_get_rate_power_limits(mphy, mphy->chandef.chan,
+					      &limits_array, tx_power);
+	mphy->txpower_cur = tx_power;
 
 	return mt7925_mcu_set_rate_txpower(mphy);
 }
